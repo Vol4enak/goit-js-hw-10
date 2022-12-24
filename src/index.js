d1522f1577e2df782a2288306eebcc0a33a4dfb6
Vol4enak <52600927+Vol4enak@users.noreply.github.com>
2022-12-24 20:21:59 +0200

    123

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+// ,capital,,population,languages
+fetch(
+  'https://restcountries.com/v3.1/name/peru?fields=capital,population,languages,flags'
+)
+  .then(response => {
+    console.log(response.json);
+
+    return response.json();
+  })
+  .then(countrie => {
+    console.log(countrie);
+  });
+  
